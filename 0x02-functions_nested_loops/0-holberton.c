c53f8e7b5edcfe16821ab4b7e49f42ed1eed184a
Juan Pablo <juanibarbo456@gmail.com>
2019-09-23 22:12:26 -0500

    1

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include"holberton.h"
+
+int main(void)
+{
+putchar('H');
+putchar('o');
+putchar('l');
+putchar('b');
+putchar('e');
+putchar('r');
+putchar('t');
+putchar('o');
+putchar('n');
+putchar('\n');
+
+return (0);
+}
+
+
+
